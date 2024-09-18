c35f2fac0159cb692d8c45147549de5282564a79
EvanQa <ivan.khri1488@gmail.com>
2024-09-18 17:40:11 +0300

    mod

--- Exercise1.py
+++ Exercise1.py
@@ -1,3 +1,7 @@
 import random
-name: str = random.choice( ["Ivan", "Yael", "Gili", "Viki"] )
+player1_name: str = input("Enter first palyer name")
+player2_name: str = input("Enter first palyer name")
+player3_name: str = input("Enter first palyer name")
+player4_name: str = input("Enter first palyer name")
+name: str = random.choice([player1_name, player2_name, player3_name, player4_name] )
 print(f'Winner is {name}')
